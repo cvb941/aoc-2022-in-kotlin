a910877b91b58e57ba14bf6d0ff5271583d97b03
Lukas Kusik <lukas.kusik@gmail.com>
2022-12-01 08:18:02 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalories = 0
+        var calorieSum = 0
+        input.forEach {
+            if (it.isEmpty()) {
+                maxCalories = maxOf(maxCalories, calorieSum)
+                calorieSum = 0
+            } else {
+                calorieSum += it.toInt()
+            }
+        }
+        return maxCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val caloriesList = mutableListOf<Int>()
+        var calorieSum = 0
+        input.forEach {
+            if (it.isEmpty()) {
+                caloriesList += calorieSum
+                calorieSum = 0
+            } else {
+                calorieSum += it.toInt()
+            }
+        }
+        return caloriesList.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
